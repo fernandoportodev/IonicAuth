f8b47851149e6d5915e2af53fd4c22d94bdf0434
Fernando Porto <fernandogomes143@gmail.com>
2024-10-09 08:52:26 -0300

    Deploy github Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth'
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { environment } from 'src/environments/environment';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
@@ -14,10 +14,30 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, 
-    AngularFireModule, AngularFireAuthModule, AngularFireModule.initializeApp(environment.firebaseConfig)
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"auth-ionic-85e0a","appId":"1:774432341487:web:02d16788724a361d934a1a","storageBucket":"auth-ionic-85e0a.appspot.com","apiKey":"AIzaSyDsGryIbKGRq3VbErmJ-ihIpGqq95R7qLs","authDomain":"auth-ionic-85e0a.firebaseapp.com","messagingSenderId":"774432341487","measurementId":"G-C4T448N0XT"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'auth-ionic-85e0a',
+        appId: '1:774432341487:web:02d16788724a361d934a1a',
+        storageBucket: 'auth-ionic-85e0a.appspot.com',
+        apiKey: 'AIzaSyDsGryIbKGRq3VbErmJ-ihIpGqq95R7qLs',
+        authDomain: 'auth-ionic-85e0a.firebaseapp.com',
+        messagingSenderId: '774432341487',
+        measurementId: 'G-C4T448N0XT',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDsGryIbKGRq3VbErmJ-ihIpGqq95R7qLs",
+    authDomain: "auth-ionic-85e0a.firebaseapp.com",
+    projectId: "auth-ionic-85e0a",
+    storageBucket: "auth-ionic-85e0a.appspot.com",
+    messagingSenderId: "774432341487",
+    appId: "1:774432341487:web:02d16788724a361d934a1a",
+    measurementId: "G-C4T448N0XT"
+  }
 };
